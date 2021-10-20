eb58aa84341d5d661f9badb938f26c0834e3ffdf
Ege <m.egegunduz@gmail.com>
2021-10-20 12:53:41 +0300

    initial setup changes

--- app/src/main/java/com/groupg/foodrecipe/ui/authentication/AuthenticationActivity.kt
+++ app/src/main/java/com/groupg/foodrecipe/ui/authentication/AuthenticationActivity.kt
@@ -7,6 +7,6 @@ import com.groupg.foodrecipe.R
 class AuthenticationActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_authentication2)
+        setContentView(R.layout.activity_authentication)
     }
 }
