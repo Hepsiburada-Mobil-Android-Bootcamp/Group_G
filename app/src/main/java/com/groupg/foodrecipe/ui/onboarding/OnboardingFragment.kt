fb20df72cf4e60aaae793244c3eb90f491e2bddc
Ege <m.egegunduz@gmail.com>
2021-10-22 01:43:32 +0300

    indicators added

--- app/src/main/java/com/groupg/foodrecipe/ui/onboarding/OnboardingFragment.kt
+++ app/src/main/java/com/groupg/foodrecipe/ui/onboarding/OnboardingFragment.kt
@@ -5,6 +5,11 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.ViewGroup.LayoutParams.WRAP_CONTENT
+import android.widget.ImageView
+import android.widget.LinearLayout
+import androidx.core.content.ContextCompat
+import androidx.viewpager2.widget.ViewPager2
 import com.groupg.foodrecipe.R
 import com.groupg.foodrecipe.adapter.OnboardingAdapter
 import com.groupg.foodrecipe.databinding.FragmentOnboardingBinding
@@ -14,6 +19,7 @@ class OnboardingFragment : Fragment() {
     private val binding get() = _binding!!
 
     private lateinit var adapter: OnboardingAdapter
+    private lateinit var indicatorsContainer: LinearLayout
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -23,12 +29,58 @@ class OnboardingFragment : Fragment() {
         _binding = FragmentOnboardingBinding.inflate(inflater, container, false)
 
         initViewPager()
+        placeIndicators()
+        setCurrentIndicator(0)
         return binding.root
     }
 
     private fun initViewPager() {
         adapter = OnboardingAdapter(requireActivity())
         binding.onboardingViewPager.adapter = adapter
+
+        binding.onboardingViewPager.registerOnPageChangeCallback(object :
+            ViewPager2.OnPageChangeCallback() {
+                override fun onPageSelected(position: Int) {
+                    super.onPageSelected(position)
+                    setCurrentIndicator(position)
+                }
+            }
+        )
+    }
+
+    private fun placeIndicators() {
+        adapter = OnboardingAdapter(requireActivity())
+        indicatorsContainer = binding.indicatorsContainer
+        val indicators = arrayOfNulls<ImageView>(adapter.itemCount)
+        val layoutParams: LinearLayout.LayoutParams = LinearLayout.LayoutParams(WRAP_CONTENT, WRAP_CONTENT)
+        val applicationContext = activity?.applicationContext!!
+        layoutParams.setMargins(16, 0, 16, 0)
+        for (i in indicators.indices) {
+            indicators[i] = ImageView(applicationContext)
+            indicators[i]?.let {
+                it.setImageDrawable(
+                    ContextCompat.getDrawable(applicationContext, R.drawable.indicator_inactive)
+                )
+                it.layoutParams = layoutParams
+                indicatorsContainer.addView(it)
+            }
+        }
+    }
+
+    private fun setCurrentIndicator(position: Int) {
+        val childCount = indicatorsContainer.childCount
+        for (i in 0 until childCount) {
+            val imageView = indicatorsContainer.getChildAt(i) as ImageView
+            if (i == position) {
+                imageView.setImageDrawable(
+                    ContextCompat.getDrawable(activity?.applicationContext!!, R.drawable.indicator_active)
+                )
+            } else {
+                imageView.setImageDrawable(
+                    ContextCompat.getDrawable(activity?.applicationContext!!, R.drawable.indicator_inactive)
+                )
+            }
+        }
     }
 
     override fun onDestroyView() {
