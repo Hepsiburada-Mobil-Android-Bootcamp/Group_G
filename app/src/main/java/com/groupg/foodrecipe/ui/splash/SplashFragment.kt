652a1709eed02f926cd90999fa0c3d5174566e7d
Ege <m.egegunduz@gmail.com>
2021-10-21 21:01:19 +0300

    Start MainActivity on splash animation end

--- app/src/main/java/com/groupg/foodrecipe/ui/splash/SplashFragment.kt
+++ app/src/main/java/com/groupg/foodrecipe/ui/splash/SplashFragment.kt
@@ -1,10 +1,13 @@
 package com.groupg.foodrecipe.ui.splash
 
+import android.animation.Animator
+import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.groupg.foodrecipe.MainActivity
 import com.groupg.foodrecipe.databinding.FragmentSplashBinding
 
 
@@ -16,10 +19,32 @@ class SplashFragment : Fragment() {
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
         _binding = FragmentSplashBinding.inflate(inflater, container, false)
 
+        controlSplashAnimation()
         return binding.root
     }
 
+    private fun controlSplashAnimation() {
+        binding.lottieAnimationSplash.addAnimatorListener(object: Animator.AnimatorListener{
+            override fun onAnimationStart(p0: Animator?) {
+
+            }
+
+            override fun onAnimationEnd(p0: Animator?) {
+                val intent = Intent(context, MainActivity::class.java)
+                startActivity(intent)
+                requireActivity().finish()
+            }
+
+            override fun onAnimationCancel(p0: Animator?) {
+
+            }
+
+            override fun onAnimationRepeat(p0: Animator?) {
+
+            }
+        })
+    }
 }
