66aa574fe707f101d3810f6e362fae40aea48648
Ege <m.egegunduz@gmail.com>
2021-10-21 18:46:32 +0300

    created splash_nav with dummy splash fragment

---
+++ app/src/main/java/com/groupg/foodrecipe/ui/splash/SplashFragment.kt
@@ -0,0 +1,25 @@
+package com.groupg.foodrecipe.ui.splash
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.groupg.foodrecipe.databinding.FragmentSplashBinding
+
+
+class SplashFragment : Fragment() {
+    private var _binding: FragmentSplashBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = FragmentSplashBinding.inflate(inflater, container, false)
+        binding.dummyTextView.text = "Dummy Text"
+        return binding.root
+    }
+
+}
