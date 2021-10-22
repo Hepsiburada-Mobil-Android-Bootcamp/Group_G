ab6615a33d280377cb499cfeba83fdcc75553742
Ege <m.egegunduz@gmail.com>
2021-10-22 19:08:10 +0300

    login validations and dummy login

--- app/src/main/java/com/groupg/foodrecipe/ui/authentication/signin/SignInFragment.kt
+++ app/src/main/java/com/groupg/foodrecipe/ui/authentication/signin/SignInFragment.kt
@@ -10,6 +10,7 @@ import androidx.navigation.fragment.findNavController
 import com.groupg.foodrecipe.MainActivity
 import com.groupg.foodrecipe.R
 import com.groupg.foodrecipe.databinding.FragmentSignInBinding
+import java.util.regex.Pattern
 
 class SignInFragment : Fragment() {
     private var _binding: FragmentSignInBinding? = null
@@ -30,10 +31,75 @@ class SignInFragment : Fragment() {
         }
 
         binding.signInButton.setOnClickListener {
+            attemptLogin()
+        }
+    }
+
+    private fun attemptLogin() {
+        val email = binding.signInEmail.text.toString()
+        val password = binding.signInPassword.text.toString()
+
+        val emailIsValid = isValidEmail(email)
+        val passwordIsValid = isValidPassword(password)
+
+        if (emailIsValid && passwordIsValid) {
             val intent = Intent(context, MainActivity::class.java)
             startActivity(intent)
             requireActivity().finish()
         }
     }
 
+    private fun isValidEmail(email: String): Boolean {
+        val input = binding.signInTextInputEmail
+        var isValid : Boolean
+
+        when {
+            email.isEmpty() -> {
+                input.error = getString(R.string.email_is_required)
+                isValid = false
+            }
+            !email.contains(".") || !email.contains("@") || email.length >= 50 -> {
+                input.error = getString(R.string.email_invalid)
+                isValid = false
+            }
+            else -> {
+                input.error = null
+                isValid = true
+            }
+        }
+        return isValid
+    }
+
+    private fun isValidPassword(password: String): Boolean {
+        val input = binding.signInTextInputPassword
+        val passwordPattern = Pattern.compile("^^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z])(?=.*[@#$%^&+=]).{7,40}$")
+        var isValid: Boolean
+
+        when {
+            password.isEmpty() -> {
+                isValid = false
+                input.error = getString(R.string.password_is_required)
+            }
+            passwordPattern.matcher(password).matches() -> {
+                isValid = true
+                input.error = null
+            }
+            else -> when {
+                password.length < 7 -> {
+                    isValid = false
+                    input.error = getString(R.string.short_password)
+                }
+                password.length > 40 -> {
+                    isValid = false
+                    input.error = getString(R.string.long_password)
+                }
+                else -> {
+                    isValid = false
+                    input.error = getString(R.string.password_restrictions)
+                }
+            }
+        }
+        return isValid
+    }
 }
+
